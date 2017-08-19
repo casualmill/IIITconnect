05e5b7467dd1d6230b8d6e01f58656b9bbf09393
nipun24 <nipunhaldar24@gmail.com>
2017-08-18 21:51:22 -0700

    Integrated email registration

--- app/src/main/java/com/casualmill/iiitconnect/MainActivity.java
+++ app/src/main/java/com/casualmill/iiitconnect/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -27,15 +29,85 @@ import com.google.firebase.auth.UserInfo;
 
 import static android.R.attr.data;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
+    //Buttons
+    Button registerButton;
+
+    //EditText Views
+    EditText mUserName,mEmail,mRollNumber,mPassword;
+
+    //firebase variables
+    private FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //firebase variables
+        mAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        //Registering details
+        mUserName = (EditText) findViewById(R.id.register_name);
+        mEmail = (EditText) findViewById(R.id.register_email);
+        mRollNumber = (EditText) findViewById(R.id.register_roll_number);
+        mPassword = (EditText) findViewById(R.id.register_password);
+
+        //Buttons
+        registerButton = (Button) findViewById(R.id.register_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register(mEmail.getText().toString(),mPassword.getText().toString());
+            }
+        });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser()!=null){
+                    startActivity(new Intent(MainActivity.this,SignedIn.class));
+                }
+            }
+        };
+
+    }
+
+    private void register(String email, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                //if registering is successful
+                if(task.isSuccessful()){
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    startActivity(new Intent(MainActivity.this,SignedIn.class));
+                }
+
+                //if registering is failed
+                else{
+                    Toast.makeText(MainActivity.this,"Registering Failed! Please Try again",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
+
     }
 }
-
 
 
 
